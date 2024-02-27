b723987d3a167eaeb5b6609865cc5dfac3eaa5c7
Mateus <mateusrochamr133@gmail.com>
2024-02-27 17:32:17 -0300

    Novas animações acrescentadas no javascript na section testemunha, estilizações novas no css da section testemunha, criação da section plano principal,estilização da section plano principal e novas animações da section plano principal no javascript

--- _scripts/script.js
+++ _scripts/script.js
@@ -9,6 +9,7 @@ $('section#domingo-perfeito').waypoint(function(direcao){
 
     }
     else if (direcao == "up") {
+        
         $('div.folha1') .removeClass('animate__animated animate__backInDown');
         $('div.folha3') .removeClass('animate__animated animate__backInDown');
         $('div.camarao') .removeClass('animate__animated animate__backInDown');
@@ -17,7 +18,93 @@ $('section#domingo-perfeito').waypoint(function(direcao){
         $('div.folha2') .removeClass('animate__animated animate__backInDown');
     }
 
-    
 }, {
-    offset:'450px'
+    offset:'450px;'
 })
+
+
+$('div#testemunhos ul:eq(0)').css('opacity', 0);
+$('div#testemunhos ul:eq(1)').css('opacity', 0);
+$('div#testemunhos ul:eq(2)').css('opacity', 0);
+
+
+$('section#testemunha').waypoint(function(direcao){
+    if(direcao == 'down') {
+        $('div#testemunhos ul:eq(0)').addClass('animate__animated animate__fadeInUp animate__slower');
+
+        $('div#testemunhos ul:eq(1)').addClass('animate__animated animate__fadeInUp animate__slow');
+
+        $('div#testemunhos ul:eq(2)').addClass('animate__animated animate__fadeInUp animate__slower');
+    }
+
+    if (direcao == 'up') {
+        $('div#testemunhos ul:eq(0)').removeClass('animate__animated animate__fadeInUp animate__slower');
+
+        $('div#testemunhos ul:eq(1)').removeClass('animate__animated animate__fadeInUp animate__slow');
+
+        $('div#testemunhos ul:eq(2)').removeClass('animate__animated animate__fadeInUp animate__slower');
+    }
+
+
+}, {
+
+    offset: '400px;'
+
+})
+
+
+$('div#transicao-testemunha-plano').waypoint(function(direcao){
+    if (direcao == 'down') {
+        $('div#decoracao1').removeClass('animate__animated animate__fadeOutLeft animate__slow');
+
+        $('div#decoracao2').removeClass('animate__animated animate__fadeOut animate__slow');
+
+        $('div#decoracao3').removeClass('animate__animated animate__fadeOutRight animate__slow');
+
+
+
+        $('div#decoracao1').addClass('animate__animated animate__fadeInLeft animate_slow');
+
+        $('div#decoracao2').addClass('animate__animated animate__fadeIn animate_slow');
+
+        $('div#decoracao3').addClass('animate__animated animate__fadeInRight animate_slow');
+
+    }
+
+    else {
+
+        $('div#decoracao1').addClass('animate__animated animate__fadeOutLeft animate_slow');
+
+        $('div#decoracao2').addClass('animate__animated animate__fadeOut animate_slow');
+
+        $('div#decoracao3').addClass('animate__animated animate__fadeOutRight animate_slow');
+    }
+} , {
+
+    offset: '600px;'
+})
+
+
+$('section#plano-principal').waypoint(function(direcao){
+    if (direcao == 'down') {
+
+        $('div.planos:eq(0)').addClass('animate__animated animate__fadeInLeft animate_slower');
+
+        $('div.planos:eq(1)').addClass('animate__animated animate__fadeInUp animate_slower');
+
+        $('div.planos:eq(2)').addClass('animate__animated animate__fadeInRight animate_slower');
+    }
+    else {
+
+        $('div.planos:eq(0)').removeClass('animate__animated animate__fadeInLeft animate_slower');
+
+        $('div.planos:eq(1)').removeClass('animate__animated animate__fadeInUp animate_slower');
+
+        $('div.planos:eq(2)').removeClass('animate__animated animate__fadeInRight animate_slower');
+    }
+}, {
+    offset: '450px;'
+})
+
+
+
